d33c7a81da4cf0244841865f0d95057c2df260e2
bougie <bougie@appartland.eu>
2015-02-16 07:37:34 +0100

    change package name on pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
     with open('README.rst') as f:
         return f.read()
 
-setup(name='elephant',
+setup(name='elephant_backup',
       version='0.1',
       description="Duplicity frontend",
       long_description=readme(),
