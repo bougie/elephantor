03359f88d9e1d5e741b3c77fe1e5ba43de03d9a0
bougie <bougie@appartland.eu>
2015-02-16 07:42:25 +0100

    change version after git merge

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
         return f.read()
 
 setup(name='elephant_backup',
-      version='0.1',
+      version='0.2',
       description="Duplicity frontend",
       long_description=readme(),
       classifiers=[
